a57436224e255427d69c4b94fb2a5090f60ecc5a
Mariza0 <maria.net@mail.ru>
2024-01-09 15:51:55 +0100

    change appveyor

--- e2e/submitCard.test.js
+++ e2e/submitCard.test.js
@@ -24,64 +24,64 @@ describe('Check cardNumber through submit', () => {
     await page.waitForTimeout(2000);
   }, 10000);
 
-    // ввод валидной карты MASTERCARD (номер сгенерирован на общедоступном сервисе https://www.vccgenerator.org/ru/visa-card-generator-result/ )
-    test('input valid number', async () => {
-      await page.goto('http://localhost:9000');
-      await page.type('input', '5524910129565442');
-      const submit = await page.$('[data-id="card-submit"]');
-      submit.click();
-      await page.$('.status-luhn.activate');
-      await page.waitForTimeout(2000);
-    }, 10000);
+  // ввод валидной карты MASTERCARD (номер сгенерирован на общедоступном сервисе https://www.vccgenerator.org/ru/visa-card-generator-result/ )
+  test('input valid number', async () => {
+    await page.goto('http://localhost:9000');
+    await page.type('input', '5524910129565442');
+    const submit = await page.$('[data-id="card-submit"]');
+    submit.click();
+    await page.$('.status-luhn.activate');
+    await page.waitForTimeout(2000);
+  }, 10000);
 
-     // ввод валидной карты JCB ((номер взят на общедоступном сервисе https://www.freeformatter.com/credit-card-number-generator-validator.html/
-     test('input valid number', async () => {
-      await page.goto('http://localhost:9000');
-      await page.type('input', '3533283112872371');
-      const submit = await page.$('[data-id="card-submit"]');
-      submit.click();
-      await page.$('.status-luhn.activate');
-      await page.waitForTimeout(2000);
-    }, 10000);
+  // ввод валидной карты JCB ((номер взят на общедоступном сервисе https://www.freeformatter.com/credit-card-number-generator-validator.html/
+  test('input valid number', async () => {
+    await page.goto('http://localhost:9000');
+    await page.type('input', '3533283112872371');
+    const submit = await page.$('[data-id="card-submit"]');
+    submit.click();
+    await page.$('.status-luhn.activate');
+    await page.waitForTimeout(2000);
+  }, 10000);
 
-    // ввод валидной карты DISCOVER (номер взят на общедоступном сервисе https://www.freeformatter.com/credit-card-number-generator-validator.html/
-    test('input valid number', async () => {
-      await page.goto('http://localhost:9000');
-      await page.type('input', '6011180540391921');
-      const submit = await page.$('[data-id="card-submit"]');
-      submit.click();
-      await page.$('.status-luhn.activate');
-      await page.waitForTimeout(2000);
-    }, 10000);
+  // ввод валидной карты DISCOVER (номер взят на общедоступном сервисе https://www.freeformatter.com/credit-card-number-generator-validator.html/
+  test('input valid number', async () => {
+    await page.goto('http://localhost:9000');
+    await page.type('input', '6011180540391921');
+    const submit = await page.$('[data-id="card-submit"]');
+    submit.click();
+    await page.$('.status-luhn.activate');
+    await page.waitForTimeout(2000);
+  }, 10000);
 
-    // ввод валидной карты American Express (номер взят на общедоступном сервисе https://www.freeformatter.com/credit-card-number-generator-validator.html/
-    test('input valid number', async () => {
-      await page.goto('http://localhost:9000');
-      await page.type('input', '344261159923708');
-      const submit = await page.$('[data-id="card-submit"]');
-      submit.click();
-      await page.$('.status-luhn.activate');
-      await page.waitForTimeout(2000);
-    }, 10000);
+  // ввод валидной карты American Express (номер взят на общедоступном сервисе https://www.freeformatter.com/credit-card-number-generator-validator.html/
+  test('input valid number', async () => {
+    await page.goto('http://localhost:9000');
+    await page.type('input', '344261159923708');
+    const submit = await page.$('[data-id="card-submit"]');
+    submit.click();
+    await page.$('.status-luhn.activate');
+    await page.waitForTimeout(2000);
+  }, 10000);
 
-     // ввод валидной карты MIR 
-     test('input valid number', async () => {
-      await page.goto('http://localhost:9000');
-      await page.type('input', '2201382000000013');
-      const submit = await page.$('[data-id="card-submit"]');
-      submit.click();
-      await page.$('.status-luhn.activate');
-      await page.waitForTimeout(2000);
-    }, 10000);
+  // ввод валидной карты MIR
+  test('input valid number', async () => {
+    await page.goto('http://localhost:9000');
+    await page.type('input', '2201382000000013');
+    const submit = await page.$('[data-id="card-submit"]');
+    submit.click();
+    await page.$('.status-luhn.activate');
+    await page.waitForTimeout(2000);
+  }, 10000);
 
-    test('input valid number', async () => {
-      await page.goto('http://localhost:9000');
-      await page.type('input', '220012345678910');
-      const submit = await page.$('[data-id="card-submit"]');
-      submit.click();
-      await page.$('.status-luhn.activate');
-      await page.waitForTimeout(2000);
-    }, 10000);
+  test('input valid number', async () => {
+    await page.goto('http://localhost:9000');
+    await page.type('input', '220012345678910');
+    const submit = await page.$('[data-id="card-submit"]');
+    submit.click();
+    await page.$('.status-luhn.activate');
+    await page.waitForTimeout(2000);
+  }, 10000);
 
   // ввод НЕвалидной карты
   test('input valid number', async () => {
@@ -91,7 +91,6 @@ describe('Check cardNumber through submit', () => {
     submit.click();
     await page.$('.status-luhn-negative.activate');
     await page.waitForTimeout(2000);
-
   }, 10000);
 
   afterAll(async () => {
