415b93b44e87f346c0d0c71445fdd85c1e6b1c07
Mariza0 <maria.net@mail.ru>
2024-01-10 01:40:45 +0100

    change headless: true

--- e2e/submitCard.test.js
+++ e2e/submitCard.test.js
@@ -6,7 +6,7 @@ describe('Check cardNumber through submit', () => {
 
   beforeAll(async () => {
     browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       slowMo: 100,
       devtools: true,
     });
